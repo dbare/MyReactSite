70e3fe9f17195afbf4531a1fc21ae40eee4d4112
Davison Bare <davis@Davisons-MacBook-Pro.local>
2017-09-14 12:38:33 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+var Hello = React.createClass({
+    render: function() {
+        return <div className="helloWorld">Hello, haters!</div>;
+    }
+});
+
+export default Hello;
+
