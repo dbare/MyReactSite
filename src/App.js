fdf8095cad275e8a7314663170ea6109c4b013af
Davison Bare <davis@Davisons-MacBook-Pro.local>
2017-09-21 12:44:33 -0700

    added inro paragraph

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import Sound from 'react-sound';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 var Hello = React.createClass({
-    render: function() {
-        return <div className="helloWorld">Hello, haters!</div>;
-    }
+  render: function() {
+    return <div><h1 className="helloWorld">Hello, haters!</h1>
+    <p>Hi!  My name is Davis.  I was born and raised just outside of Philadelphia and lived there until 2014, when I moved to Oakland,CA.</p></div>;
+      }
 });
 
-export default Hello;
 
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Sound from 'react-sound';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
